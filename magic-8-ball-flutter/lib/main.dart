c17a5eaa516524bed9888d3af71adb014f5495ff
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-01 15:07:31 +0100

    Added an appbar and container to the body

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
