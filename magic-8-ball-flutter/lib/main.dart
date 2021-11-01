63b35166de68116a21addb8f730e73a92452491c
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-01 15:43:00 +0100

    Image changes implemented using dart math library

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -25,17 +26,22 @@ class BallPage extends StatelessWidget {
 
 class Ball extends StatefulWidget {
   const Ball({Key? key}) : super(key: key);
-
   @override
   _BallState createState() => _BallState();
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Image.asset('images/ball1.png'),
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+          child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
