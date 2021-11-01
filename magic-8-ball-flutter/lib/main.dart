dc4aa22ef39b40ca07e41e72936332ecb85cd26c
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-01 15:14:59 +0100

    Added the ball image to the statefull widget container

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -13,12 +13,29 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text('Ask Me Anything'),
       ),
-      body: Container(
-        color: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset('images/ball1.png'),
       ),
     );
   }
